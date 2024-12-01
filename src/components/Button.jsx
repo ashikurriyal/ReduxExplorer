e1d20676f5d4dcd8c4b4d45629b7f7541de74179
ashikurriyal <riyal.ashikur@gmail.com>
2024-12-01 22:52:46 +0600

    button configaration updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,18 @@
 /* eslint-disable react/prop-types */
 
-
 const Button = ({ children, type, handle }) => {
-    const style = type === 'danger'
-        ? 'bg-red-500 text-white px-3 py-2 rounded-lg shadow'
-        : 'bg-green-500 text-white px-3 py-2 rounded-lg shadow'; // Fixed typo 'tex' to 'text'
+  const style =
+    type === "danger"
+      ? "bg-red-500 text-white px-3 py-2 rounded-lg shadow"
+      : "bg-green-500 text-white px-3 py-2 rounded-lg shadow"; // Fixed typo 'tex' to 'text'
 
-    return (
-        <button className={style} onClick={handle}>
-            {children}
-        </button>
-    );
+  return (
+    <div>
+      <button className={style} onClick={handle}>
+        {children}
+      </button>
+    </div>
+  );
 };
 
 export default Button;
